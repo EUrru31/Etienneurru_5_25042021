a9d66ad091c56b2fdb87bec620c934e727d4d520
Urru <etienne.urru@gmail.com>
2021-04-19 14:34:07 +0200

    Page principale JS

--- Front-End/index.js
+++ Front-End/index.js
@@ -8,6 +8,7 @@ async function main() {
     }
 }
 
+// Récupération de la base de donnée
 async function getAllTeddies() {
     return fetch("http://localhost:3000/api/teddies")
         .then(function (httpBodyResponse) {
@@ -15,9 +16,13 @@ async function getAllTeddies() {
         })
         .then(function (teddies) {
             return teddies;
+        })
+        .catch((error) => {
+            alert("Connexion échouée !");
         });
 }
 
+// Alimentation du template
 function displayTeddy(teddy) {
     const templateElt = document.getElementById("templateTeddy");
     const cloneElt = document.importNode(templateElt.content, true);
@@ -26,8 +31,16 @@ function displayTeddy(teddy) {
     cloneElt.getElementById("teddies__description").textContent =
         teddy.description;
     cloneElt.getElementById("teddies__price").textContent = teddy.price;
-    /*cloneElt.getElementById("teddies__imageUrl").setAttribute("");
-    cloneElt.getElementById("teddies__url").setAttribute();*/
+    cloneElt.getElementById("teddies__imageUrl").src = teddy.imageUrl;
+
+    cloneElt
+        .getElementById("teddies__url")
+        .setAttribute(
+            "src",
+            "http://127.0.0.1:5500/pageproduit.html&id=" + teddy.id
+        );
 
     document.getElementById("main").appendChild(cloneElt);
+
+    //const euro =new Intl.NumberFormat('fr', { style: 'currency', currency: 'EUR' }).format(number));
 }
